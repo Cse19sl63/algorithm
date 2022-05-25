11bec8f8bacd6c18537b6d4661570f7fcfd9e956
mudaser ahmad lone <91317290+Cse19sl63@users.noreply.github.com>
2022-05-25 11:19:32 +0530

    Add files via upload

---
+++ mergesort.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#define max 10
+
+int a[11]={10,14,19,26,31,33,35,42,44,0};
+int b[10];
+void merging(int lb,int mid,int ub)
+{
+	int l1,l2,i;
+	for(l1=lb,l2=mid+1,i=lb;l1<=mid && l2<=ub;i++)
+	{
+		if(a[l1]<=a[12])
+		b[i]=a[l1++];
+	
+	else
+	b[i]=a[l2++];
+}
+while(l1<=mid)
+b[i++]=a[l1++];
+while(l2<=ub)
+b[i++]=a[l2++];
+for(i=lb;i<ub;i++)
+a[i]=b[i];
+}
+void sort(int lb,int ub)
+{
+	int mid;
+	if(lb<ub)
+	{
+		mid=(lb+ub)/2;
+		sort(lb,mid);
+		sort(mid+1,ub);
+		merging(lb,mid,ub);
+		
+	}
+	else
+	{
+		return;
+	}
+}
+int main()
+
+{
+	int i;
+
+	printf("list before sorting\n");
+	for(i=0;i<=max;i++)
+	printf("%d ", a[i]);
+	sort(0,max);
+             printf("\nlist after the sorting\n");
+	for( i =0;i <= max; i++)
+	printf("%d ", a[i]);
+	
+}
+
+
+
+
+
+
+
+
